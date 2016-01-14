7db9880664a849310e58f8d3f711c7d5b90ece95
sebbalazs <balazs_adrian@yahoo.it>
2016-01-15 00:18:22 +0100

    update on preferences

---
+++ app/src/main/java/com/group5/wewander/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.group5.wewander;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+/**
+ * Created by Eigenaar on 15-1-2016.
+ */
+public class LoginActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final Button button = (Button) findViewById(R.id.loginButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // if click go to user profile
+
+            }
+        });
+
+    }
+
+
+}
--- app/src/main/java/com/group5/wewander/UserProfile.java
+++ app/src/main/java/com/group5/wewander/UserProfile.java
@@ -1,14 +1,37 @@
 package com.group5.wewander;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.MapFragment;
+
 /**
  * Created by Sebastian on 14-1-2016.
  */
-public class UserProfile {
+public class UserProfile extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        final Button button = (Button) findViewById(R.id.Save_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // if click go to main page
+
+            }
+        });
+
+
+    }
 }
