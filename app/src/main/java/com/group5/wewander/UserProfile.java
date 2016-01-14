271ae3f5e989788f0210e8d8d132ce5f61bbbd71
sebbalazs <balazs_adrian@yahoo.it>
2016-01-14 23:46:07 +0100

    Added user profile and login activity

---
+++ app/src/main/java/com/group5/wewander/UserProfile.java
@@ -0,0 +1,14 @@
+package com.group5.wewander;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+/**
+ * Created by Sebastian on 14-1-2016.
+ */
+public class UserProfile {
+
+}
